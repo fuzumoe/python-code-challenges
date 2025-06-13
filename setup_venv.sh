24f316551c68fb4d27fe553912f44672b5b32da0
Adam Fuzum <atew@camelot-itlab.com>
2025-06-13 21:36:58 +0200

    fix(venv) adjust venv name to .venv

--- setup_venv.sh
+++ setup_venv.sh
@@ -6,12 +6,12 @@ then
     echo "Python3 is not installed. Please install it before running this script."
     exit
 fi
-sudo  apt install python3.12-venv -y
+sudo  apt install python3-venv -y
 # Create a virtual environment
-python3 -m venv venv
+python3 -m venv .venv
 
 # Activate the virtual environment
-source venv/bin/activate
+source .venv/bin/activate
 
 # Upgrade pip
 pip install --upgrade pip
@@ -21,4 +21,4 @@ pip install --upgrade pip
 pip install -r requirements.txt
 
 echo "Python virtual environment is created, packages are installed, and requirements.txt is generated."
-echo "To activate the virtual environment, use: source venv/bin/activate"
+echo "To activate the virtual environment, use: source .venv/bin/activate"
